419babfc7208e05cd194db44045f17f2b26b916e
Bert Riffelmacher <sotsoguk@gmail.com>
2023-12-04 14:05:02 +0100

    Day04 Python solved

--- python/day01/day01.py
+++ python/day01/day01.py
@@ -1,6 +1,7 @@
 import os
 import time
 import heapq
+import re
 
 def main():
 
@@ -8,19 +9,19 @@ def main():
     print(os.getcwd())
     day = "01"
     year = "2023"
-    input_file = f'../inputs/day{day}.txt'
+    input_file = f'inputs/day{day}.txt'
     print(input_file)
     part1, part2 = 0, 0
     with open(input_file) as f:
-        groups = f.read().split('\n\n')
-
+        lines = f.read().splitlines()
+    print(lines)
     start_time = time.time()
     # calc
-    calories = sorted([sum(list(map(int, group.splitlines())))
-                      for group in groups])
-    part1 = calories[-1]
-    part2 = sum(calories[-3:])
-
+    for l in lines:
+        dl = re.findall("\d",l)
+        # print(int("".join([dl[0],dl[-1]])))
+        print(dl)
+        part1 += int("".join([dl[0],dl[-1]]))
     # output
     duration = int((time.time() - start_time) * 1000000)
     header = "#" * 20
---
+++ python/day04/day04.py
@@ -0,0 +1,44 @@
+import os
+import time
+import heapq
+import re
+from math import floor
+
+
+def main():
+    # input
+    print(os.getcwd())
+    day = "04"
+    year = "2023"
+    input_file = f"inputs/day{day}.txt"
+    print(input_file)
+    part1, part2 = 0, 0
+    with open(input_file) as f:
+        lines = f.read().splitlines()
+
+    start_time = time.time()
+    cards = [1] * len(lines)
+    print(cards)
+    
+    # calc
+    for k, l in enumerate(lines):
+        w, y = l.split(":")[1].split("|")
+        ws = set([int(i.strip()) for i in w.strip().split(" ") if i.isdigit()])
+        ys = set([int(i.strip()) for i in y.strip().split(" ") if i.isdigit()])
+        numberWinningCards = len(ws.intersection(ys))
+        pointsWinningCards = floor(pow(2, numberWinningCards - 1))
+        for j in range(1, numberWinningCards + 1):
+            cards[k + j] += cards[k]
+        part1 += pointsWinningCards
+    part2 = sum(cards)
+    
+    # output
+    duration = int((time.time() - start_time) * 1000000)
+    header = "#" * 20
+    print(
+        f"{header}\n*AoC {year} - Day {day} *\n{header}\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nTime:\t{duration // 1000} ms"
+    )
+
+
+if __name__ == "__main__":
+    main()
